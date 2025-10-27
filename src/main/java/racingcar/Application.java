b3caf49559393c4ea2ef591ae0495b5d9f01f725
codms0201 <chanyeol0615@naver.com>
2025-10-27 23:04:53 +0900

    feat(app): Application 메인 진입점에 모든 구성 요소 통합 - InputView, RacingGame, RandomNumberGenerator 연결 - IllegalArgumentException 예외를 적절하게 처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import racingcar.domain.RacingGame;
+import racingcar.utils.DefaultRandomNumberGenerator;
+import racingcar.utils.NumberGenerator;
+import racingcar.view.InputView;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+       try {
+           List<String> names = InputView.readCarNames();
+           int attempts = InputView.readAttempCount();
+           NumberGenerator generator = new DefaultRandomNumberGenerator();
+           RacingGame game = new RacingGame(names, attempts, generator);
+           game.play();
+       } catch (IllegalArgumentException e) {
+           System.out.println(e.getMessage());
+       }
     }
 }

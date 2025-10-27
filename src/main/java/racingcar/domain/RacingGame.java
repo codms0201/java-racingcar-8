bedc30e1770a93dfeeb9cc4b4be6e3d8fb03c72d
codms0201 <chanyeol0615@naver.com>
2025-10-27 22:56:28 +0900

    feat(game): RacingGame 메인 흐름 구현 - 라운드를 실행하고 우승자를 결정하는 play() 메서드 추가 - 진행 과정 및 결과 표시를 위해 OutputView와 통합

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,4 +1,28 @@
 package racingcar.domain;
 
+import racingcar.view.OutputView;
+import racingcar.utils.NumberGenerator;
+
+import java.util.List;
+
 public class RacingGame {
+    private final Cars cars;
+    private final int attempts;
+    private final NumberGenerator generator;
+
+    public RacingGame(List<String> names, int attempts, NumberGenerator generator) {
+        this.cars = new Cars(names);
+        this.attempts = attempts;
+        this.generator = generator;
+    }
+
+    public List<String> play() {
+        for (int i = 0; i < attempts; i++) {
+            cars.playRound(generator);
+            OutputView.printRoundResult(cars.getCars());
+        }
+        List<String> winners = cars.findWinners();
+        OutputView.printWinners(winners);
+        return winners;
+    }
 }

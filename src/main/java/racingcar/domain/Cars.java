9ab40786ec6e378afbd0f1cf92ff8d5b9aad729e
codms0201 <chanyeol0615@naver.com>
2025-10-27 21:19:13 +0900

    feat(domain): 여러 Car 객체를 관리하는 Cars 클래스 추가 - NumberGenerator를 사용하는 playRound() 추가 - 가장 앞선 위치의 자동차 이름을 반환하는 findWinners() 추가

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,4 +1,40 @@
 package racingcar.domain;
 
+import racingcar.utils.NumberGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Cars {
+    private final List<Car> cars;
+
+    public Cars(List<String> names) {
+        this.cars = new ArrayList<>();
+        for (String name : names) {
+            cars.add(new Car(name));
+        }
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void playRound(NumberGenerator generator) {
+        for (Car car : cars) {
+            int number = generator.generate();
+            car.move(number);
+        }
+    }
+
+    public List<String> findWinners() {
+        int max = cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+        return cars.stream()
+                .filter(c -> c.getPosition() == max)
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
 }
